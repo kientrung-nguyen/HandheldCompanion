8b04bb3474c0de8bab51005de6aa87c8a47ed60a
Kien Trung Nguyen <kientrung.nguyen@gmail.com>
2024-09-16 23:46:04 +0800

    checkpoint

---
+++ HandheldCompanion/Converters/StringEmptyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace HandheldCompanion.Converters
+{
+    public class StringEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? parameter : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
